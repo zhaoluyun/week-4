adb9aa30013ee6e549f7d8c218a074b53aa6a631
Nathan Zimmerman <npzimmerman@gmail.com>
2016-02-08 22:30:13 -0500

    Improve in-code instructions for lab2

--- lab/lab2/js/part3-main.js
+++ lab/lab2/js/part3-main.js
@@ -1,29 +1,33 @@
 /* =====================
-   You should NOT need to change this file
+   You should NOT need to change this file (though you are not forbidden from doing so)
 ===================== */
 
 /* =====================
-  Call getData to grab our dataset through a jQuery.ajax call ($.ajax)
+  Call getAndParseData to grab our dataset through a jQuery.ajax call ($.ajax)
 ===================== */
-getData();
+getAndParseData();
 
 /* =====================
   The code here is triggered when you click on the button with ID #my-button
-  ALL functions called here will be called EVERY time a clicke event fires
+  ALL functions called here will be called EVERY time a click event fires
 ===================== */
 $('button#my-button').click(function(e) {
   numericField1 = $('#num1').val();
-  numericField2 = $('#num2').val();
-  booleanField = $('#boolean')[0].checked;
-  stringField = $('#string').val();
   console.log("numericField1", numericField1);
+
+  numericField2 = $('#num2').val();
   console.log("numericField2", numericField2);
+
+  booleanField = $('#boolean')[0].checked;
   console.log("booleanField", booleanField);
+
+  stringField = $('#string').val();
   console.log("stringField", stringField);
 
 
   /* =====================
-    Call our resetMap function to remove markers from the map and clear our the array of markers
+    Call our resetMap function to remove markers from the map and clear out the array of marker
+    objects
   ===================== */
   resetMap();
 
